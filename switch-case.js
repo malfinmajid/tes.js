b01d0560b83435c2bda3eca5a3d4ff4d3d46fd49
Muhammad Alfin Majid <malfinnmajid@gmail.com>
2025-04-27 12:58:51 +0700

    tes4

---
+++ switch-case.js
@@ -0,0 +1,15 @@
+const lampuLalulintas = "Kuning";
+
+switch(lampuLalulintas){
+    case 'Merah':
+        console.log("Berhenti");
+        break;
+    case 'Kuning':
+        console.log("Bersiap");
+        break;
+    case 'Hijau':
+        console.log("Berjalan");
+        break;
+    default:
+        console.log("Lampu Mati");
+}
