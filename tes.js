26e7a6f31892bc0935c34a2a182e64ced01065a6
Muhammad Alfin Majid <malfinnmajid@gmail.com>
2025-04-27 12:54:25 +0700

    tes

---
+++ tes.js
@@ -0,0 +1,15 @@
+const lampuLalulintas = "Merah";
+
+switch(lampuLalulintas){
+    case 'Merah':
+        console.log("Berhenti");
+        break;
+    case 'Kuning':
+        console.log("Bersiap");
+        break;
+    case 'Hijau':
+        console.log("Jalan");
+        break;
+    default:
+        console.log("Lampu Mati");
+}
